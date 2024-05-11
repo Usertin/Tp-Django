a8508814c51ec42bdfc0e42dca9017ebda125a0f
Test <zarzismehdi7@gmail.com>
2024-05-11 20:38:06 +0100

    swapped the home page for a slicker one

--- jeux/views.py
+++ jeux/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('ceci est la page de jeux')
+    return HttpResponse("ceci est la page index de jeux<br><a href='/'>go back</a>")
